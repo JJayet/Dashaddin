425a29e24f7bfe0c8c8f0793dc8bf005303bef51
Jonathan Jayet <jonathan@jayet.eu>
2015-11-16 23:12:49 +0100

    Better parsing of the DOM in order to fetch relevant inputs

--- popup.js
+++ popup.js
@@ -1,38 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.getSelected(null, function(tab) {
-    function doStuffWithDom(domContent) {
-      var wrapper= document.createElement('div');
-      wrapper.innerHTML= domContent;
-      var pwdInputs = wrapper.querySelectorAll('input[type=password]');
-      var textInputs = wrapper.querySelectorAll('input[type=text]');
-      
-      var pwdDiv = document.createElement('div');
+    var dom;
+    var pwdTerms = ['password', 'pass', 'pwd'];
+    var userTerms = ['e-mail', 'email', 'user', 'username'];
 
-      [].forEach.call(pwdInputs, function(elem) {
-        pwdDiv.appendChild(document.createTextNode(elem.outerHTML));
-        pwdDiv.appendChild(document.createElement('br'));  
-      });
+    function getInputsAndContext(domContent) {
+      dom = document.createElement('div');
+      dom.innerHTML = domContent;
+      getInputs();
+    }
 
+    function getContextForInput(input) {
+      if(input.type == "password") {
+        return {input : input, context : "Password"};
+      }
+      return {input : input, context : "???"};
+    }
+
+    function getLabelForId(id) {
+      console.log(dom.querySelector("label[for='" + id + "']"));
+    }
+
+    function getContextFromInputs(inputs) {
+
+    }
+
+    function getInputs() {
+      var textInputs = dom.querySelectorAll('input:not([type="radio"]):not([type="hidden"])');
       var textDiv = document.createElement('div');
-      
-      [].forEach.call(textInputs, function(elem) {
-        textDiv.appendChild(document.createTextNode(elem.outerHTML));
-        textDiv.appendChild(document.createElement('br'));  
-      });
 
-      var pwdTitle = document.createElement('h2');
-      pwdTitle.innerHTML = "Password inputs"
-      document.body.appendChild(pwdTitle);
-      document.body.appendChild(pwdDiv);
+      [].forEach.call(textInputs, function(elem) {
+        var inputAndContext = getContextForInput(elem);
+        var text = document.createTextNode(inputAndContext.input.outerHTML + ", " + inputAndContext.context);
+        textDiv.appendChild(text);
+        textDiv.appendChild(document.createElement('br'));
+      });
 
       var textTitle = document.createElement('h2');
-      textTitle.innerHTML = "Text inputs"
+      textTitle.innerHTML = "Inputs"
       document.body.appendChild(textTitle);
       document.body.appendChild(textDiv);
     }
 
-    chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
-    
+    chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, getInputsAndContext);
+
   });
 }, false);
-
